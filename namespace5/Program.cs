1b4f96d7d132e9f2fc99f2a4397908192caf631f
we1dy <ladyarboleda26@gmail.com>
2024-08-28 01:51:50 +0800

    Add project files.

---
+++ namespace5/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Program
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			int p, lastInt = 0, input;
+			Console.WriteLine("Enter the Number of Rows: ");
+			input = int.Parse(Console.ReadLine());
+
+			for (int i = 1; i <= input; i++)
+			{
+				for (p = 1; p <= i; p++)
+				{
+					if (lastInt == 1)
+					{
+						Console.Write("0");
+						lastInt = 0;
+					}
+					else
+					{
+						Console.Write("1");
+						lastInt = 1;
+					}
+				}
+				Console.Write("\n");
+			}
+
+			Console.ReadLine();
+		}
+	}
+}
